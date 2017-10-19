eb4dda609444e4a0e241effd293043f18a85b4f4
AlexSvetenco <asvetenco@yahoo.com>
2017-10-19 23:26:15 +0300

    tag

--- AnnimationTest/app/src/main/java/sa/anim/MainActivity.java
+++ AnnimationTest/app/src/main/java/sa/anim/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
